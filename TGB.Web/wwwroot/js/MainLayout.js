1171167603378fb581fdfcb20ffd4caee4cc563b
cjraynerrose <cjraynerrose@gmail.com>
2024-09-16 19:45:02 +0100

    wip

---
+++ TGB.Web/wwwroot/js/MainLayout.js
@@ -0,0 +1,41 @@
+﻿const sideMenuOpenKey = 'SIDE_MENU_OPEN';
+
+function ApplyLastSlideState() {
+    var slideState = sessionStorage.getItem(sideMenuOpenKey);
+
+    if (slideState === 'false') {
+        SideSlideHidden()
+        var sideMenu = document.getElementById('sideMenu');
+        sideMenu.classList.remove('show');
+    }
+}
+
+function SideSlideShown() {
+    let toggleExpandIcon = document.getElementById('toggleExpand');
+
+    toggleExpandIcon.classList.add('fa-minimize');
+    toggleExpandIcon.classList.remove('fa-expand');
+    sessionStorage.setItem(sideMenuOpenKey, 'true');
+}
+
+function SideSlideHidden() {
+    let toggleExpandIcon = document.getElementById('toggleExpand');
+
+    toggleExpandIcon.classList.add('fa-expand');
+    toggleExpandIcon.classList.remove('fa-minimize');
+    sessionStorage.setItem(sideMenuOpenKey, 'false');
+}
+
+$(document).ready(function () {
+    var sideMenu = document.getElementById('sideMenu');
+
+    sideMenu.addEventListener('hide.bs.collapse', () => {
+        SideSlideHidden();
+    });
+
+    sideMenu.addEventListener('show.bs.collapse', () => {
+        SideSlideShown();
+    });
+
+    ApplyLastSlideState();
+});
